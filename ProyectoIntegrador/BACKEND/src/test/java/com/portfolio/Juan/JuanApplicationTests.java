65d37d76ef955a3d38e253b55eb50c6b9fed90a3
Juanikilador <juanpro1581.4@gmail.com>
2023-01-14 17:41:51 -0300

    Backend

---
+++ ProyectoIntegrador/BACKEND/src/test/java/com/portfolio/Juan/JuanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.Juan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
